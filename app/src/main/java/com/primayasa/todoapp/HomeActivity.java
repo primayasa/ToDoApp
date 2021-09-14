555f3b2efe0e5cf9db8aabf900e34e8b7db50472
primayasa <75405558+primayasa@users.noreply.github.com>
2021-09-14 20:11:05 +0700

    first commit

---
+++ app/src/main/java/com/primayasa/todoapp/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.primayasa.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/primayasa/todoapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.primayasa.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int waktu_loading=1000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
